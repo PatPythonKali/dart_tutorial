c8473510491a65e4aa4a9d1b9bcd8a242432c912
ubuntu-server <patpythonkali@gmail.com>
2021-08-18 02:15:59 +0800

    Updates

---
+++ 2. Getting Started/2-2-Comments.dart
@@ -0,0 +1,14 @@
+void main(){
+
+  // Print Strings
+  print("Hello World!");
+  print("This is my first application!");
+
+  // Print Arithmatic Operation
+  print(4 - 1);
+
+  // Print Booleans
+  print(true);
+  print(false);
+
+}
---
+++ 3. Exploring Variables and Data Types/3-1-Data-Types-And-Variables.dart
@@ -0,0 +1,22 @@
+void main(){
+  // Number: int
+  int score = 23;
+  var score2 = 24;
+
+  // Number: float
+  double percentage = 93.4;
+  var percentage2 = 93.5;
+
+  // Boolean
+  bool isAlive = true;
+  var isAlive2 = true;
+
+  print(score);
+  print(score2);
+
+  print(percentage);
+  print(percentage2);
+
+  print(isAlive);
+  print(isAlive2);
+}
