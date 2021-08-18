1be10f35a0e841734536155ec66f65d6da21de3b
ubuntu-server <patpythonkali@gmail.com>
2021-08-18 20:28:30 +0800

    Updates

---
+++ 5. Loop Control Statements/5-3-While-Loop.dart
@@ -0,0 +1,19 @@
+void main(){
+  // While Loop
+  var i =1;
+  while (i <= 10){
+    print(i);
+    i++;
+  }
+
+  // While Loop Even
+  var x = 1;
+  while(x <= 10){
+    if (x %2 == 0){
+      print(x);
+
+    }
+    x++;
+
+  }
+}
