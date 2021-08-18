c35f3a06cc80a97122b17d34d628966f16dacd10
ubuntu-server <patpythonkali@gmail.com>
2021-08-18 13:54:36 +0800

    Updates

---
+++ 3. Exploring Variables and Data Types/3-3-Define-Constants-using-final-and-const.dart
@@ -0,0 +1,21 @@
+void main(){
+  // Final keyword
+  final cityName = "Mumbai";
+
+  // Optional
+  final String countryName = "India";
+
+  // cityName = "Delhi";
+
+  // Const keyword
+  const PI = 3.14;
+
+  // Optional
+  const double gravity = 9.8;
+
+}
+
+class Circle {
+  final String color = 'Red';
+  static const PI = 3.14;
+}
