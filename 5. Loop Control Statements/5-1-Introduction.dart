ab31e841d4cc55a5f1ea562b61ee4cb4635052d7
ubuntu-server <patpythonkali@gmail.com>
2021-08-18 19:40:27 +0800

    Updates

---
+++ 5. Loop Control Statements/5-1-Introduction.dart
@@ -0,0 +1,18 @@
+void main(){
+  // FOR Loop
+  for (int x = 1; x <= 10; x++){
+    print("Hello X");
+  }
+  // WHILE Loop
+  int z = 1;
+  while(z < 10){
+    print("Hello Z");
+    z++;
+  }
+  // DO Loop
+  int y = 1;
+  do{
+    print("Hello Y");
+    y++;
+  } while (y < 10);
+}
