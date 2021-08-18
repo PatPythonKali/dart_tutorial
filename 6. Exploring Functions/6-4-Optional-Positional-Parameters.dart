df9f0d4aa9c379616bb1f45e1b55e4c3ee41ce1a
ubuntu-server <patpythonkali@gmail.com>
2021-08-19 00:23:11 +0800

    Updates

---
+++ 6. Exploring Functions/6-4-Optional-Positional-Parameters.dart
@@ -0,0 +1,18 @@
+void main(){
+  printCities("New York", "New Delhi", "Sydney");
+  print("");
+  
+  printCountries("USA", "Canada", "Philippines");
+}
+
+void printCities(String name1, String name2, String name3){
+  print("Name 1 is $name1");
+  print("Name 2 is $name2");
+  print("Name 3 is $name3");
+}
+
+void printCountries(String name1, String name2, String name3){
+  print("Name 1 is $name1");
+  print("Name 2 is $name2");
+  print("Name 3 is $name3");  
+}
---
+++ 6. Exploring Functions/6-5-Optional-Named-Parameter.dart
@@ -0,0 +1,7 @@
+void main(){
+  var result = findVolume(2, 3,  2);
+}
+
+int findVolume(int length, int breadth, int height ){
+  return length * breadth * height;
+}
