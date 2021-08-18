50291c40ac6fe7d922ba74d14fda9f4b126c69be
ubuntu-server <patpythonkali@gmail.com>
2021-08-18 15:53:40 +0800

    Updates

--- 4. Control Flow Statements/4-3-Switch-Case-Conditional.dart
+++ 4. Control Flow Statements/4-3-Switch-Case-Conditional.dart
@@ -1,3 +1,22 @@
 void main(){
+  String grade = '123';
+
+  // Switch only accepts int or String
+  switch(grade) {
+    case 'A':
+      print("Excellent Grade A!");
+      break;
+
+    case 'B':
+      print("Very Good!");
+      break;
+
+    case 'C':
+      print("Good Enough. Work Harder!");
+      break;
+
+    default:
+      print("You have failed! ");
+  }
 
 }
